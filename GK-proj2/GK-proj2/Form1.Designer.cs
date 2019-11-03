2257088de2132caa62dd5ea274f598b718f1e6e0
seba <seba@DESKTOP-0OGOCFP>
2019-11-03 20:21:55 +0100

    initial

---
+++ GK-proj2/GK-proj2/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace GK_proj2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.DrawArea = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.AddPoly = new System.Windows.Forms.Button();
+            this.DeletePoly = new System.Windows.Forms.Button();
+            this.EditPoly = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.StartB = new System.Windows.Forms.Button();
+            this.LightXY = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LightZC = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SpeedC = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LightZC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(729, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(729, 488);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel2.Controls.Add(this.DrawArea, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel2, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 53);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(723, 432);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // DrawArea
+            // 
+            this.DrawArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DrawArea.Location = new System.Drawing.Point(3, 3);
+            this.DrawArea.Name = "DrawArea";
+            this.DrawArea.Size = new System.Drawing.Size(567, 426);
+            this.DrawArea.TabIndex = 0;
+            this.DrawArea.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawArea_Paint);
+            this.DrawArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DrawArea_MouseDown);
+            this.DrawArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.DrawArea_MouseUp);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.AddPoly);
+            this.flowLayoutPanel1.Controls.Add(this.DeletePoly);
+            this.flowLayoutPanel1.Controls.Add(this.EditPoly);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(723, 44);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // AddPoly
+            // 
+            this.AddPoly.Location = new System.Drawing.Point(3, 3);
+            this.AddPoly.Name = "AddPoly";
+            this.AddPoly.Size = new System.Drawing.Size(62, 41);
+            this.AddPoly.TabIndex = 0;
+            this.AddPoly.Text = "Add polygon";
+            this.AddPoly.UseVisualStyleBackColor = true;
+            this.AddPoly.Click += new System.EventHandler(this.AddPoly_Click);
+            // 
+            // DeletePoly
+            // 
+            this.DeletePoly.Location = new System.Drawing.Point(71, 3);
+            this.DeletePoly.Name = "DeletePoly";
+            this.DeletePoly.Size = new System.Drawing.Size(62, 41);
+            this.DeletePoly.TabIndex = 1;
+            this.DeletePoly.Text = "Delete polygon";
+            this.DeletePoly.UseVisualStyleBackColor = true;
+            this.DeletePoly.Click += new System.EventHandler(this.DeletePoly_Click);
+            // 
+            // EditPoly
+            // 
+            this.EditPoly.Location = new System.Drawing.Point(139, 3);
+            this.EditPoly.Name = "EditPoly";
+            this.EditPoly.Size = new System.Drawing.Size(62, 41);
+            this.EditPoly.TabIndex = 2;
+            this.EditPoly.Text = "Move polygon";
+            this.EditPoly.UseVisualStyleBackColor = true;
+            this.EditPoly.Click += new System.EventHandler(this.EditPoly_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel2.Controls.Add(this.StartB);
+            this.flowLayoutPanel2.Controls.Add(this.LightXY);
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.LightZC);
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Controls.Add(this.SpeedC);
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(576, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(144, 426);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // StartB
+            // 
+            this.StartB.Location = new System.Drawing.Point(3, 3);
+            this.StartB.Name = "StartB";
+            this.StartB.Size = new System.Drawing.Size(141, 52);
+            this.StartB.TabIndex = 0;
+            this.StartB.Text = "Start";
+            this.StartB.UseVisualStyleBackColor = true;
+            this.StartB.Click += new System.EventHandler(this.StartB_Click);
+            // 
+            // LightXY
+            // 
+            this.LightXY.Location = new System.Drawing.Point(3, 61);
+            this.LightXY.Name = "LightXY";
+            this.LightXY.Size = new System.Drawing.Size(141, 52);
+            this.LightXY.TabIndex = 1;
+            this.LightXY.Text = "Choose light position";
+            this.LightXY.UseVisualStyleBackColor = true;
+            this.LightXY.Click += new System.EventHandler(this.LightXY_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // LightZC
+            // 
+            this.LightZC.Location = new System.Drawing.Point(3, 132);
+            this.LightZC.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.LightZC.Name = "LightZC";
+            this.LightZC.Size = new System.Drawing.Size(141, 20);
+            this.LightZC.TabIndex = 2;
+            this.LightZC.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.LightZC.ValueChanged += new System.EventHandler(this.LightZC_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Light Z";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Speed";
+            // 
+            // SpeedC
+            // 
+            this.SpeedC.Location = new System.Drawing.Point(3, 171);
+            this.SpeedC.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.SpeedC.Name = "SpeedC";
+            this.SpeedC.Size = new System.Drawing.Size(141, 20);
+            this.SpeedC.TabIndex = 5;
+            this.SpeedC.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.SpeedC.ValueChanged += new System.EventHandler(this.SpeedC_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 515);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Polygon Animation";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LightZC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel DrawArea;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button AddPoly;
+        private System.Windows.Forms.Button DeletePoly;
+        private System.Windows.Forms.Button EditPoly;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button StartB;
+        private System.Windows.Forms.Button LightXY;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown LightZC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown SpeedC;
+    }
+}
+
---
+++ GK-proj2/GK-proj2/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GK_proj2
+{
+    public partial class Form1 : Form
+    {
+        bool IsMouseDown = false;
+        List<Polygon> polygons = new List<Polygon>();
+        List<Polygon> MovingPolygons = new List<Polygon>();
+        public enum Mode { AddPolygon, Move, Delete, Light }
+        Mode CurrentMode;
+        Polygon WhichPoly;
+        int? WhichPoint;
+        Point LightP = new Point(0,0);
+        int LightZ = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CurrentMode = Mode.AddPolygon;
+            int x = DrawArea.ClientSize.Width;
+            MovingPolygons.Add(new Polygon(new Point(200, 200)));
+            MovingPolygons[MovingPolygons.Count - 1].points.Add(new Point(250, 250));
+            MovingPolygons[MovingPolygons.Count - 1].points.Add(new Point(200, 250));
+            MovingPolygons[MovingPolygons.Count - 1].Completed = true;
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void line(int x, int y, int x2, int y2, SolidBrush sb, Graphics g) //https://stackoverflow.com/questions/11678693/all-cases-covered-bresenhams-line-algorithm
+        {
+            int w = x2 - x;
+            int h = y2 - y;
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+            if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+            if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+            if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+            int longest = Math.Abs(w);
+            int shortest = Math.Abs(h);
+            if (!(longest > shortest))
+            {
+                longest = Math.Abs(h);
+                shortest = Math.Abs(w);
+                if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+                dx2 = 0;
+            }
+            int numerator = longest >> 1;
+            for (int i = 0; i <= longest; i++)
+            {
+                g.FillRectangle(sb, x, y, 1, 1);
+                numerator += shortest;
+                if (!(numerator < longest))
+                {
+                    numerator -= longest;
+                    x += dx1;
+                    y += dy1;
+                }
+                else
+                {
+                    x += dx2;
+                    y += dy2;
+                }
+            }
+        }
+
+        void drawVertice(Point p, SolidBrush sb, Graphics g)
+        {
+            for (int i = -3; i <= 3; i++)
+                g.FillRectangle(sb, p.X - i, p.Y - 3, 1, 1);
+            for (int i = -3; i <= 3; i++)
+                g.FillRectangle(sb, p.X - i, p.Y + 3, 1, 1);
+            for (int i = -2; i <= 2; i++)
+                g.FillRectangle(sb, p.X - 3, p.Y - i, 1, 1);
+            for (int i = -3; i <= 3; i++)
+                g.FillRectangle(sb, p.X + 3, p.Y - i, 1, 1);
+        }
+
+        private void DrawArea_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Polygon p in MovingPolygons)
+            {
+
+                if (p.Completed)
+                {
+                    for (int i = 0; i < p.points.Count; i++)
+                    {
+                        line(p.points[i].X, p.points[i].Y, p.points[(i + 1) % p.points.Count].X, p.points[(i + 1) % p.points.Count].Y, new SolidBrush(Color.Black), e.Graphics);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < p.points.Count - 1; i++)
+                    {
+                        line(p.points[i].X, p.points[i].Y, p.points[(i + 1) % p.points.Count].X, p.points[(i + 1) % p.points.Count].Y, new SolidBrush(Color.Black), e.Graphics);
+                    }
+                }
+            }
+
+
+
+            drawVertice(LightP, new SolidBrush(Color.Red), e.Graphics);
+            foreach (Polygon p in polygons)
+            {
+                //draw poly
+                for (int i = 0; i < p.points.Count; i++)
+                    drawVertice(p.points[i], new SolidBrush(Color.Black), e.Graphics);
+
+                if (p.Completed)
+                {
+                    for (int i = 0; i < p.points.Count; i++)
+                    {
+                        line(p.points[i].X, p.points[i].Y, p.points[(i + 1) % p.points.Count].X, p.points[(i + 1) % p.points.Count].Y, new SolidBrush(Color.Black), e.Graphics);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < p.points.Count - 1; i++)
+                    {
+                        line(p.points[i].X, p.points[i].Y, p.points[(i + 1) % p.points.Count].X, p.points[(i + 1) % p.points.Count].Y, new SolidBrush(Color.Black), e.Graphics);
+                    }
+                }
+            }
+        }
+
+        private void DrawArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (CurrentMode)
+            {
+                case Mode.AddPolygon:
+                    if (polygons.Count != 0)
+                    {
+                        Polygon p = polygons[polygons.Count - 1];
+                        if (p.Completed)
+                        {
+                            polygons.Add(new Polygon(e.Location));
+                        }
+                        else
+                        {
+                            bool Found = false;
+                            for (int i = 0; i < polygons[polygons.Count - 1].points.Count && !Found; i++)
+                            {
+                                if ((Math.Abs(e.X - p.points[i].X) < 10 && Math.Abs(e.Y - p.points[i].Y) < 10))
+                                {
+                                    Found = true;
+                                }
+                            }
+                            if(p.points.Count >= 3 && Math.Abs(e.X - p.points[0].X) < 10 && Math.Abs(e.Y - p.points[0].Y) < 10)
+                            {
+                                p.Completed = true;
+                            }
+                            else
+                            {
+                                polygons[polygons.Count - 1].points.Add(e.Location);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        polygons.Add(new Polygon(e.Location));
+                    }
+                    break;
+                case Mode.Delete:
+                    for (int i = 0; i < polygons.Count; i++)
+                    {
+                        if(polygons[i].IsInside(e.Location))
+                        {
+                            polygons.RemoveAt(i);
+                            break;
+                        }
+                    }
+                    break;
+                case Mode.Move:
+                    bool Found1 = false;
+                    for (int i = 0; i < polygons.Count && !Found1; i++)
+                    {
+                        for (int j = 0; j < polygons[i].points.Count && !Found1; j++)
+                        {
+                            if (Math.Abs(e.X - polygons[i].points[j].X) < 10 && Math.Abs(e.Y - polygons[i].points[j].Y) < 10)
+                            {
+                                Found1 = true;
+                                WhichPoly = polygons[i];
+                                WhichPoint = j;
+                                break;
+                            }
+                        }
+                    }
+                    for (int i = 0; i < polygons.Count && !Found1; i++)
+                    {
+                        if (polygons[i].IsInside(e.Location))
+                        {
+                            WhichPoly = polygons[i];
+                            break;
+                        }
+                    }
+                    break;
+                case Mode.Light:
+                    LightP = e.Location;
+                    break;
+            }
+            DrawArea.Refresh();
+        }
+
+        private void DrawArea_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch(CurrentMode)
+            {
+                case Mode.Move:
+                    if (WhichPoly != null)
+                    {
+                        if (WhichPoint != null)
+                        {
+                            WhichPoly.points[(int)WhichPoint] = e.Location;
+                            WhichPoly = null;
+                            WhichPoint = null;
+                        }
+                        else
+                        {
+                            Point middle1 = WhichPoly.Middle();
+                            for (int i = 0; i < WhichPoly.points.Count; i++)
+                            {
+                                WhichPoly.points[i] = new Point(WhichPoly.points[i].X + (e.X - middle1.X), WhichPoly.points[i].Y + (e.Y - middle1.Y));
+                            }
+                            WhichPoly = null;
+                        }
+                    }
+                    break;
+            }
+            DrawArea.Refresh();
+        }
+
+        private void AddPoly_Click(object sender, EventArgs e)
+        {
+            CurrentMode = Mode.AddPolygon;
+        }
+
+        private void DeletePoly_Click(object sender, EventArgs e)
+        {
+            CurrentMode = Mode.Delete;
+        }
+
+        private void EditPoly_Click(object sender, EventArgs e)
+        {
+            CurrentMode = Mode.Move;
+        }
+
+        private void LightXY_Click(object sender, EventArgs e)
+        {
+            CurrentMode = Mode.Light;
+        }
+
+        private void StartB_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                StartB.Text = "Start";
+                timer1.Stop();
+            }
+            else
+            {
+                StartB.Text = "Stop";
+                timer1.Start();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (var p in MovingPolygons)
+            {
+                for (int i = 0; i < p.points.Count; i++)
+                {
+                    p.points[i] = new Point(p.points[i].X - 1, p.points[i].Y); 
+                }
+            }
+            DrawArea.Refresh();
+        }
+
+        private void SpeedC_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)SpeedC.Value;
+        }
+
+        private void LightZC_ValueChanged(object sender, EventArgs e)
+        {
+            LightZ = (int)LightZC.Value;
+        }
+    }
+}
